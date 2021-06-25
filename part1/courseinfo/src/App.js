cccdd70e83b991c06df3aa0df5c58ea1a490bbbf
wade <macaw@macaws-MacBook-Pro.local>
2021-06-25 20:59:29 +0300

    1.3 - 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -15,35 +15,53 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
+  let partsComponent = []
+  props.parts.forEach((item) => {
+    partsComponent.push(<Part name={item.name} ex={item.exercises} />)  
+  })
+
   return (
-    <div>
-      <Part name={props.p1} ex={props.ex1} />
-      <Part name={props.p2} ex={props.ex2} />
-      <Part name={props.p3} ex={props.ex3} />
-    </div>
+    <>
+      {partsComponent}
+    </>
   )
 }
 
 const Total = (props) => {
+  let total = 0
+  props.parts.forEach((item) => {
+    total += item.exercises
+  })
+
   return (
-    <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>
+    <p>Number of exercises {total}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 12
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header name={course} />
-      <Content p1={part1} ex1={exercises1} p2={part2} ex2={exercises2} p3={part3} ex3={exercises3} />
-      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
