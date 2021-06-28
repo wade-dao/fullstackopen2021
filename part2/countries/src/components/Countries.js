a5ee39cb3a92fe1ffc964755c6010dd82d6cfbde
wade <macaw@macaws-MacBook-Pro.local>
2021-06-28 22:49:45 +0300

    2.14

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 const Country = ({country}) => {
   const [showDetails, setShowDetails] = useState(false)
@@ -28,21 +29,8 @@ const Country = ({country}) => {
         </table>
       </td>
     </tr>
-      
   )
 }
-
-// const CountryButton = ({country}) => {
-//   const [showDetails, setShowDetails] = useState(false)
-
-
-//   return (
-//     <>
-//       <button id={country.name} onClick={onClick}>{showDetails? "show" : "hide"}</button>
-//       {showDetails ? <CountryDetail country={country} /> : null}
-//     </>
-//   ) 
-// }
 
 const CountryDetail = ({country}) => {
   console.log('country ', country)
@@ -66,16 +54,40 @@ const CountryDetail = ({country}) => {
               <td>{country.population}</td>
             </tr>
             <tr>
-              <h2>Languages</h2>
+              <td>
+                <h2>Languages</h2>
+              </td>
             </tr>
             <tr>
-              <ul>
-                {languages}
-              </ul>
+              <td>
+                <ul>
+                  {languages}
+                </ul>
+              </td>
             </tr>
             <tr>
               <td>
                 <img width="247" height="130" src={country.flag} alt="country flag"/>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h2>Weather in {country.capital}</h2>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p><strong>temperature:</strong> {country.weather.current.temperature} Celsius</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <img src={country.weather.current.weather_icons[0]} alt="weather icon"/>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p><strong>wind:</strong> {country.weather.current.wind_speed} {country.weather.request.unit}ph direction {country.weather.current.wind_dir}</p>
               </td>
             </tr>
           </tbody>
@@ -86,6 +98,7 @@ const CountryDetail = ({country}) => {
 }
 
 const Countries = ({countries}) => {
+  const api_key = process.env.REACT_APP_WEATHER_STACK_API_KEY
   if (countries.length > 10)
     return (
       <p>Too many matches, specify another filter</p>
@@ -98,7 +111,21 @@ const Countries = ({countries}) => {
       </div>
     )
   
-  const list = countries.map((country, index) => <Country key={index} country={country} />)
+  const list = countries.map((country, index) => {
+    axios
+      .get('http://api.weatherstack.com/current', {
+        params: {
+          access_key: api_key,
+          query: country.capital
+        }
+      })
+      .then(response => {
+        console.log('weather retrieved', response.data)
+        country.weather = response.data
+      })
+
+    return <Country key={index} country={country} />
+  })
   return (
     <table>
       <tbody>
