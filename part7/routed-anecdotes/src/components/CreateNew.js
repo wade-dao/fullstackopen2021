03cb5c4af7335a4d8004c49af899e02f5e7ee965
wade <macaw@macaws-MacBook-Pro.local>
2021-07-19 00:46:01 +0300

    7.4 - 7.6

--- part7/routed-anecdotes/src/components/CreateNew.js
+++ part7/routed-anecdotes/src/components/CreateNew.js
@@ -1,18 +1,30 @@
 import React, { useState } from 'react'
+import { useField } from '../hooks/index'
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+
+  const { clearValue: clearValueContent, ...contentInput} = content
+  const { clearValue: clearValueAuthor, ...authorInput} = author
+  const { clearValue: clearValueInfo, ...infoInput} = info
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
+  }
+
+  const handleReset = (e) => {
+    clearValueContent(e)
+    clearValueAuthor(e)
+    clearValueInfo(e)
   }
 
   return (
@@ -21,17 +33,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' {...contentInput} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author' {...authorInput} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input name='info' {...infoInput} />
         </div>
         <button>create</button>
+        <button type='button' onClick={handleReset}>reset</button>
       </form>
     </div>
   )
---
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const clearValue = (event) => {
+    setValue('')
+    event.target.value = ''
+  }
+
+  return {
+    type,
+    value,
+    onChange,
+    clearValue
+  }
+}
