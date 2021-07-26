9ff0b20574d46432dbc362e824dc658ba4628528
wade <macaw@macaws-MacBook-Pro.local>
2021-07-26 13:55:13 +0300

    8.16

--- part8/books-and-authors/index.js
+++ part8/books-and-authors/index.js
@@ -1,7 +1,12 @@
-const { ApolloServer, UserInputError, gql } = require('apollo-server')
+const { ApolloServer, UserInputError, AuthenticationError, gql } = require('apollo-server')
 const mongoose = require('mongoose')
+
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = 'THIS_IS_MY_SECRET'
+
 const Book = require('./models/book')
 const Author = require('./models/author')
+const User = require('./models/user')
 
 const MONGODB_URI = 'mongodb+srv://loud:mongopassword@cluster0.84msz.mongodb.net/library?retryWrites=true&w=majority'
 
@@ -102,10 +107,17 @@ const resolvers = {
         }
       }
     },
-    allAuthors: async () => await Author.find({})
+    allAuthors: async () => await Author.find({}),
+    me: (root, args, context) => {
+      return context.currentUser
+    }
   },
   Mutation: {
-    addBook: async (root, args) => {
+    addBook: async (root, args, { currentUser }) => {
+      if (!currentUser) {
+        throw new AuthenticationError("not authenticated")
+      }
+
       const existingAuthor = await Author.findOne({ name: args.author })
       let book
       if (existingAuthor) {
@@ -144,7 +156,11 @@ const resolvers = {
 
       return book
     },
-    editAuthor: async (root, args) => {
+    editAuthor: async (root, args, { currentUser }) => {
+      if (!currentUser) {
+        throw new AuthenticationError("not authenticated")
+      }
+
       const author = await Author.findOne({ name: args.name })
       author.born = args.setBornTo
 
@@ -156,13 +172,53 @@ const resolvers = {
         })
       }
       return author
-    }
+    },
+    createUser: async (root, args) => {
+      const user = new User({
+        username: args.username,
+        favoriteGenre: args.favoriteGenre
+      })
+  
+      try {
+        await user.save()
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        })
+      }
+
+      return user
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+  
+      if ( !user || args.password !== 'secret' ) {
+        throw new UserInputError("wrong credentials")
+      }
+  
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      }
+  
+      return { value: jwt.sign(userForToken, JWT_SECRET) }
+    },
   }
 }
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.toLowerCase().startsWith('bearer ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7), JWT_SECRET
+      )
+      const currentUser = await User.findById(decodedToken.id)
+      return { currentUser }
+    }
+  }
 })
 
 server.listen().then(({ url }) => {
--- part8/books-and-authors/models/token.js
+++ part8/books-and-authors/models/token.js
@@ -2,16 +2,11 @@ const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
 const schema = new mongoose.Schema({
-  username: {
+  value: {
     type: String,
     required: true,
-    unique: true,
-    minlength: 3
-  },
-  favoriteGenre: {
-    type: String
-  },
+    unique: true
+  }
 })
 schema.plugin(uniqueValidator)
-module.exports = mongoose.model('User', schema)
-
+module.exports = mongoose.model('Token', schema)
